f894d3ddc1717d09031e025b2790da36ea8f007b
Sonntd271 <itssonntd271@gmail.com>
2023-12-09 15:43:26 +0100

    Fixed misspelling

--- sysbench-test.sh
+++ sysbench-test.sh
@@ -5,7 +5,7 @@ sudo sysbench --test=cpu --cpu-max-prime=200000 run
 
 # Testing with an increasing number of threads
 for each in 1 2 4 8 16 32 64; do 
-    sudo sysbench --test=cpu --cpu-maxprime=200000 --num-threads=$each run
+    sudo sysbench --test=cpu --cpu-max-prime=200000 --num-threads=$each run
 done
 
 # Testing by creating files (Total 24GB)
@@ -14,5 +14,5 @@ sudo sysbench --test=fileio --file-total-size=8G --file-num=3 prepare
 # Testing by creating files with an increasing number of threads
 for each in 1 4 8 16 32; do 
     sudo sysbench --test=fileio --file-total-size=8G --file-test-mode=rndwr \
-    --max-time=240 --max-requests=0 --file-blocksize=4K --file-num=3 --num-threads=$each run
+    --max-time=240 --max-requests=0 --file-block-size=4K --file-num=3 --num-threads=$each run
 done
